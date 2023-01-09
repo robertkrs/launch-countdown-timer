9ff1a27806ba854d103e9bd0a4507dd3d541f554
Robert Reis <robertreis323@gmail.com>
2023-01-08 23:16:54 -0300

    commit final

--- js/script.js
+++ js/script.js
@@ -1,59 +1,44 @@
+const data = new Date("jan 17, 2023 14:00:00").getTime();
 const segundo = document.querySelector("[data-segundo]");
 const minuto = document.querySelector("[data-minuto]");
 const hora = document.querySelector("[data-hora]");
 const dia = document.querySelector("[data-dia]");
 
-function timerSegundo(segundo, minuto, hora, dia) {
-  segundo.innerHTML = parseInt(segundo.innerHTML);
-  segundo.innerHTML -= 1;
-  if (segundo.innerHTML > 0) {
-    setTimeout(() => {
-      timerSegundo(segundo, minuto, hora, dia);
-    }, 100);
-  } else {
-    timerMinuto(segundo, minuto, hora, dia);
-  }
-}
+let timer = setInterval(() => {
+  let tempo = new Date().getTime();
 
-function timerMinuto(segundo, minuto, hora, dia) {
-  minuto.innerHTML = parseInt(minuto.innerHTML);
-  minuto.innerHTML -= 1;
-  segundo.innerHTML = 60;
+  let resultado = data - tempo;
 
-  if (minuto.innerHTML == 0) {
-    timerHora(segundo, minuto, hora, dia);
-  } else {
-    setTimeout(() => {
-      timerSegundo(segundo, minuto, hora, dia);
-    }, 100);
-  }
-}
+  let dias = Math.floor(resultado / (1000 * 60 * 60 * 24));
+  let horas = Math.floor(
+    (resultado % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutos = Math.floor((resultado % (1000 * 60 * 60)) / (1000 * 60));
+  let segundos = Math.floor((resultado % (1000 * 60)) / 1000);
 
-function timerHora(segundo, minuto, hora, dia) {
-  hora.innerHTML = parseInt(hora.innerHTML);
-  hora.innerHTML -= 1;
-  minuto.innerHTML = 59;
-  if (hora.innerHTML == 0) {
-    timerDia(segundo, minuto, hora, dia);
-  } else {
-    setTimeout(() => {
-      timerSegundo(segundo, minuto, hora, dia);
-    }, 100);
-  }
-}
+  dia.innerHTML = dias;
+  hora.innerHTML = horas;
+  minuto.innerHTML = minutos;
+  segundo.innerHTML = segundos;
 
-function timerDia(segundo, minuto, hora, dia) {
-  dia.innerHTML = parseInt(dia.innerHTML);
-  dia.innerHTML -= 1;
-  hora.innerHTML = 23;
-  if (dia.innerHTML == 0) {
-    console.log("YEES");
-  } else {
-    setTimeout(() => {
-      timerSegundo(segundo, minuto, hora, dia);
-    }, 100);
+  if (dia.innerHTML.length === 1) {
+    dia.innerHTML = "0" + dias;
   }
-}
-setTimeout(() => {
-  timerSegundo(segundo, minuto, hora, dia);
-}, 100);
+  if (hora.innerHTML.length === 1) {
+    hora.innerHTML = "0" + horas;
+  }
+  if (minuto.innerHTML.length === 1) {
+    minuto.innerHTML = "0" + minutos;
+  }
+  if (segundo.innerHTML.length === 1) {
+    segundo.innerHTML = "0" + segundos;
+  }
+
+  if (resultado < 0) {
+    clearInterval(timer);
+    dia.innerHTML = "00";
+    hora.innerHTML = "00";
+    minuto.innerHTML = "00";
+    segundo.innerHTML = "00";
+  }
+});
